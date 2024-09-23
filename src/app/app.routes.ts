c838d3cd10d56d0d71335d8f904fff70d03cf030
jfredysoto <121842276+jfredysoto@users.noreply.github.com>
2024-09-23 20:36:49 +0000

    modificacion para editar perfil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { EditarPerfilComponent } from './editarperfil/editar-perfil.component'; // Importa el componente
 
 export const routes: Routes = [
   {
-    path: 'app-login', component: LoginComponent
+    path: 'app-login', 
+    component: LoginComponent // Ruta para la página de inicio de sesión
+  },
+  {
+    path: 'editar-perfil', 
+    component: EditarPerfilComponent // Nueva ruta para el formulario de "Editar Perfil"
   }
 ];
+
+
---
+++ src/app/editarperfil/editar-perfil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-perfil',
+  templateUrl: './editar-perfil.component.html'
+})
+export class EditarPerfilComponent {
+  // Aquí puedes manejar la lógica del formulario, como enviar los datos al backend
+  
+}
+
+
